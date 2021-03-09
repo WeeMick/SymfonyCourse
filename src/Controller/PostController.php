4f66300ac0dcf50784ec8ab72dc8f404fc4115bc
WeeMick <michaelblr5@gmail.com>
2021-03-09 15:51:40 +0000

    show and delete routes added and working

---
+++ src/Controller/PostController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PostController
+ * @package App\Controller
+ * @Route("/post", name="post.")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return Response
+     */
+    public function create(Request $request) {
+        // create new post with title
+        $post = new Post();
+        $post->setTitle('This will be title');
+
+        // entity manager
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+
+        // return a response
+        return new Response('Post was created');
+
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param $id
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function show($id, PostRepository $postRepository) {
+
+        $post = $postRepository->find($id);
+
+        // create the show view
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param $id
+     * @param PostRepository $postRepository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function remove($id, PostRepository $postRepository) {
+        $post = $postRepository->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+}
